35e1dc06d170bed8829c547f97c531e6e6c9094a
mattbarackman <matthew.barackman@gmail.com>
2013-06-06 09:54:53 -0700

    Set up skeleton for crud routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,21 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create' do
+end
+
+post '/create' do 
+end
+
+get '/:post_id' do |post_id|
+end
+
+delete '/:post_id' do |post_id|
+end
+
+get '/edit/:post_id' do |post_id|
+end
+
+post '/edit/:post_id' do |post_id|
+end
