b5e38495f6bf12ebadddd0be12976c9c8f5514ee
mattbarackman <matthew.barackman@gmail.com>
2013-06-06 09:57:48 -0700

    Create empty view files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,19 +4,25 @@ get '/' do
 end
 
 get '/create' do
+  erb :create
 end
 
 post '/create' do 
+  redirect "/#{post.id}"
 end
 
 get '/:post_id' do |post_id|
+  erb :post
 end
 
 delete '/:post_id' do |post_id|
+  redirect '/'
 end
 
 get '/edit/:post_id' do |post_id|
+  erb :edit
 end
 
 post '/edit/:post_id' do |post_id|
+  redirect "/#{post.id}"
 end
